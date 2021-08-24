2b0bc044f8e85f11bdee96015d785f1ee3111d84
Jamal Abdul Nasir <nasier.jamal@gmail.com>
2021-08-25 06:33:40 +0700

    Third commit

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
 
-func sqrt() {
-	fmt.Println("Hello, world.")
+	"github.com/viciious/go-tarantool"
+)
+
+func main() {
+	opts := tarantool.Options{User: "guest"}
+	conn, err := tarantool.Connect("127.0.0.1:3301", &opts)
+	if err != nil {
+		fmt.Printf("Connection refused: %s\n", err.Error())
+		return
+	}
+
+	query := &tarantool.Insert{Space: "examples", Tuple: []interface{}{uint64(99999), "BB"}}
+	resp := conn.Exec(context.Background(), query)
+
+	if resp.Error != nil {
+		fmt.Println("Insert failed", resp.Error)
+	} else {
+		fmt.Println(fmt.Sprintf("Insert succeeded: %#v", resp.Data))
+	}
+
+	conn.Close()
 }
